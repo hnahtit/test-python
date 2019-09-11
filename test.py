9eab9c1ef6960e81f9e024b745ba4709168c1ac8
hnahtit <ngtrungthanhcntt@gmail.com>
2019-09-11 20:23:19 +0700

    numpy array

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import os
+import numpy as np
 
-os.listdir(".")
+# a = np.full((2,2),7)
+# b = np.random.randint(low = 4,high = 6,size = (4,4))
+# print(b)
+a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+print(a)
+a_row = a[:,1]
+print(a_row)
+ind_bool = a > 2
+print(ind_bool)
